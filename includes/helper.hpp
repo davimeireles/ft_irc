5445b3f80755b772611546c80083ca6dfe3cfdf8
Davi Nunes <davimeirelespn@gmail.com>
2024-10-10 19:44:16 +0100

    prettier fix

--- includes/helper.hpp
+++ includes/helper.hpp
@@ -3,8 +3,8 @@
 #include "utils.hpp"
 
 /**
- * @brief Fucntion to check if the port was inside the range (6660-6669 , 7000)
- * and check if it was only numbers.
+ ** Fucntion to check if the port was inside the range (6660-6669 , 7000)
+ ** and check if it was only numbers.
  *
  * @param port String with the port to be checked.
  * @return true
@@ -13,18 +13,18 @@
 bool checkValidPort(string port);
 
 /**
- * @brief Function that prints the error mensage and exit the code.
+ ** Function that prints the error mensage and exit the code.
  *
  * @param error String with the error to be printed.
  */
 void p_error(string error);
 
 /**
- * @brief Function that check if it was a strong password.
- * (-1 Upper Case Letter)
- * (-1 Lower Case Letter)
- * (-1 Number)
- * (At least 8 characters)
+ ** Function that check if it was a strong password.
+ ** (-1 Upper Case Letter)
+ ** (-1 Lower Case Letter)
+ ** (-1 Number)
+ ** (At least 8 characters)
  *
  * @param password String with the password to be checked.
  * @return true
